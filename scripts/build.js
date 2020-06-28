4c0d39628cb6f99d27dd8fa8e42858d127675068
cipchk <cipchk@qq.com>
2020-06-28 19:55:27 +0800

    chore: fix build

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,16 @@ const libPackagePath = path.resolve(root, `./lib/package.json`);
 // Update version
 const rootPackageJson = require(path.resolve(root, `./package.json`));
 const libPackageJson = require(libPackagePath);
-libPackageJson.version = rootPackageJson.version;
+[
+  'version',
+  'description',
+  'keywords',
+  'author',
+  'license',
+  'repository',
+  'bugs',
+  'homepage',
+].forEach((key) => (libPackageJson[key] = rootPackageJson[key]));
 fs.writeJsonSync(libPackagePath, libPackageJson, { spaces: 2 });
 
 ngPackage
@@ -16,8 +25,13 @@ ngPackage
   .forProject(path.resolve(root, `./lib/ng-package.json`))
   .withTsConfig(path.resolve(root, './lib/tsconfig.lib.json'))
   .build()
-  .then(() => fs.copyFileSync(path.resolve(root, `./README.md`), path.resolve(root, `./publish/README.md`)))
-  .catch(error => {
+  .then(() =>
+    fs.copyFileSync(
+      path.resolve(root, `./README.md`),
+      path.resolve(root, `./publish/README.md`),
+    ),
+  )
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
