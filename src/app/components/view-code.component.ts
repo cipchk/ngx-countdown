91627030944174c4dd11f347da7a5a666a7c00d2
cipchk <cipchk@qq.com>
2024-11-24 00:06:55 +0800

    chore: update

--- src/app/components/view-code.component.ts
+++ src/app/components/view-code.component.ts
@@ -1,12 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'view-code',
   template: `
-    (<a href="https://github.com/cipchk/ngx-countdown/blob/master/src/app/components/{{ name }}.component.ts" target="_blank">View Code</a>)
+    (<a href="https://github.com/cipchk/ngx-countdown/blob/master/src/app/components/{{ name() }}.component.ts" target="_blank">View Code</a
+    >)
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewCodeComponent {
-  @Input() name = '';
+  readonly name = input('');
 }
